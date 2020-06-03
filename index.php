04f4382803b7fb93e1b20a1b5da53ed9adb9220c
Sidnei - Inventti <sidnei@inventti.com.br>
2020-06-02 22:09:02 -0300

    autoload do scarmor

--- index.php
+++ index.php
@@ -2,15 +2,17 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \scarmomr\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new scarmomr\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	$page = new Page();
+	$page->setTpl("index");    
+
 });
 
 $app->run();
